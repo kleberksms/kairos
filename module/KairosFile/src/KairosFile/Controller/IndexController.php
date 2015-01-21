0e058c53a8bb48df4fb4924fe5267c96cc802506
Kleber <sistemas.kleber@bellinatiperez.com.br>
2015-01-21 16:01:35 -0200

    IndexController para todos os controler, ele vão agir de forma independente

---
+++ module/KairosFile/src/KairosFile/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bellinati
+ * Date: 21/01/2015
+ * Time: 16:00
+ */
+
+namespace KairosFile\Controller;
+
+
+class IndexController {
+
+} 
---
+++ module/KairosUser/src/KairosUser/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bellinati
+ * Date: 21/01/2015
+ * Time: 16:01
+ */
+
+namespace KairosUser\Controller;
+
+
+class IndexController {
+
+} 

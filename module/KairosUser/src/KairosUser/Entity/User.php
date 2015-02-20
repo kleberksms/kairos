7c46a1ecb4aa44516887557c6f5d0948ba44a238
kleberksms <kleberksms@gmail.com>
2015-02-20 07:38:28 -0200

    removendo os abstracts de user

--- module/KairosUser/src/KairosUser/Entity/User.php
+++ module/KairosUser/src/KairosUser/Entity/User.php
@@ -1,15 +1,24 @@
 <?php
 namespace KairosUser\Entity;
 use Doctrine\ORM\Mapping as ORM;
+use Zend\Stdlib\Hydrator\ClassMethods;
+
 /**
  * Class User
  * @package KairosUser\Entity
  * @ORM\Entity(repositoryClass="KairosUser\Entity\UserRepository")
  * @ORM\Table(name="users")
  */
-class User extends AbstractEntity implements UserInterface
+class User  implements UserInterface
 {
 
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
 
 
     /**
@@ -35,6 +44,51 @@ class User extends AbstractEntity implements UserInterface
      * @ORM\Column
      */
     private $email;
+
+    /**
+     * @var \Datetime
+     * @ORM\Column(type="datetime")
+     */
+    protected $created;
+
+    /**
+     * @var \Datetime
+     * @ORM\Column(type="datetime")
+     */
+    protected $modified;
+
+    /**
+     * @var string
+     */
+    private $timezone;
+
+    /**
+     * @var bool
+     */
+    private $localized = false;
+
+    public function __construct(\DateTime $createDate)
+    {
+        $this->localized = true;
+        $this->created = $createDate;
+        $this->timezone = $createDate->getTimeZone()->getName();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
 
 
@@ -103,5 +157,39 @@ class User extends AbstractEntity implements UserInterface
     }
 
 
+
+    public function getCreated()
+    {
+        if (!$this->localized) {
+            $this->created->setTimeZone(new \DateTimeZone($this->timezone));
+        }
+        return $this->created;
+    }
+
+    /**
+     * @return \Datetime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+
+    public function setModified()
+    {
+        $this->modified = new \DateTime("now");
+    }
+
+    public function hydrator($content)
+    {
+        $hydrator = new ClassMethods();
+        return $hydrator->hydrate($content, $this);
+    }
+
+    public function toArray($object)
+    {
+        $hydrator = new ClassMethods();
+        return $hydrator->extract($object);
+    }
 
 } 
